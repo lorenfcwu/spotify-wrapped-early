ced8b050a281193a2f8dd77154a393298eadeb13
Loren Wu <loren13wu@gmail.com>
2023-05-23 19:01:57 -0700

    Completed Playlists section on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,7 +6,12 @@ import {
   getTopArtists,
   getTopTracks,
 } from "../spotify";
-import { SectionWrapper, ArtistsGrid, TrackList } from "../components";
+import {
+  SectionWrapper,
+  ArtistsGrid,
+  TrackList,
+  PlaylistsGrid,
+} from "../components";
 import { StyledHeader } from "../styles";
 
 const Profile = () => {
@@ -66,7 +71,7 @@ const Profile = () => {
         </StyledHeader>
       )}
 
-      {topArtists && topTracks && (
+      {topArtists && topTracks && playlists && (
         <main>
           <SectionWrapper
             title="Top artists this month"
@@ -80,6 +85,10 @@ const Profile = () => {
             seeAllLink="./top-tracks"
           >
             <TrackList tracks={topTracks.items.slice(0, 10)} />
+          </SectionWrapper>
+
+          <SectionWrapper title="Playlists" seeAllLink="/playlists">
+            <PlaylistsGrid playlists={playlists.items.slice(0, 10)} />
           </SectionWrapper>
         </main>
       )}
